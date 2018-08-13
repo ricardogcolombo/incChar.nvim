deb96a3396ed53d480e10e3b4decb2bf9cfae269
ricardogcolombo <ricardogcolombo@gmail.com>
2018-08-13 08:47:51 +0200

    [ADD]DecLetter

--- rplugin/node/incLetter.nvim/src/index.js
+++ rplugin/node/incLetter.nvim/src/index.js
@@ -1,15 +1,17 @@
-module.exports = plugin => {
+module.exports = (plugin) => {
     const nvim = plugin.nvim;
-
+    plugin.setOptions({
+        dev: true,
+    });
     function onBufWrite() {
         console.log("Buffer written!");
     }
 
     plugin.registerAutocmd("BufWritePre", onBufWrite, {
-        pattern: "*"
+        pattern: "*",
     });
 
-    function changeLetter(s) {
+    function getNext(s) {
         return s.replace(/([a-zA-Z])[^a-zA-Z]*$/, function(a) {
             var c = a.charCodeAt(0);
             switch (c) {
@@ -22,29 +24,77 @@ module.exports = plugin => {
             }
         });
     }
+
+    function getPrev(s) {
+        return s.replace(/([a-zA-Z])[^a-zA-Z]*$/, function(a) {
+            var c = a.charCodeAt(0);
+            switch (c) {
+                case 65:
+                    return "Z";
+                case 97:
+                    return "z";
+                default:
+                    return String.fromCharCode(--c);
+            }
+        });
+    }
+
     async function nextLetter(args, range) {
         // a
         const currentPos = await plugin.nvim.window.cursor;
         const buffer = await plugin.nvim.buffer;
         let lines = await buffer.getLines({
             start: currentPos[0] - 1,
-            end: currentPos[0]
+            end: currentPos[0],
         });
-        debugger;
         if (lines.length > 0) {
             let newLine = lines[0];
             let letter = newLine[currentPos[1]];
-            let nextLetter = changeLetter(letter);
-            newLine = [newLine.slice(0, currentPos[1]) + nextLetter + newLine.slice(currentPos[1] + 1, newLine.length)]
+            let nextLetter = getNext(letter);
+            newLine = [
+                newLine.slice(0, currentPos[1]) +
+                    nextLetter +
+                    newLine.slice(currentPos[1] + 1, newLine.length),
+            ];
             await buffer.setLines(newLine, {
                 start: currentPos[0] - 1,
-                end: currentPos[0]
+                end: currentPos[0],
+            });
+        }
+    }
+
+    async function prevLetter(args, range) {
+        // a
+        const currentPos = await plugin.nvim.window.cursor;
+        const buffer = await plugin.nvim.buffer;
+        let lines = await buffer.getLines({
+            start: currentPos[0] - 1,
+            end: currentPos[0],
+        });
+        if (lines.length > 0) {
+            let newLine = lines[0];
+            let letter = newLine[currentPos[1]];
+            let nextLetter = getPrev(letter);
+            newLine = [
+                newLine.slice(0, currentPos[1]) +
+                    nextLetter +
+                    newLine.slice(currentPos[1] + 1, newLine.length),
+            ];
+            await buffer.setLines(newLine, {
+                start: currentPos[0] - 1,
+                end: currentPos[0],
             });
         }
     }
     plugin.registerCommand("IncLetter", nextLetter, {
         sync: false,
         range: "",
-        nargs: "*"
+        nargs: "*",
+    });
+
+    plugin.registerCommand("DecLetter", nextLetter, {
+        sync: false,
+        range: "",
+        nargs: "*",
     });
 };
